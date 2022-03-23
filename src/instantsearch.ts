87c1f0621cfb93a9b5ed76e773737bc4056e03c5
eswaranthangavel23 <eswaran.thangavel@changecx.com>
2022-03-23 15:43:51 +0530

    Changes fixed

--- src/instantsearch.ts
+++ src/instantsearch.ts
@@ -48,7 +48,7 @@ search.addWidgets([
         image:item.images[0].url,
         category:"Furniture",
         comments: "comments",
-        sale: 12,
+        sale:`$ ${(item.price.centAmount / 100)}.00`,
         // eslint-disable-next-line @typescript-eslint/camelcase
         sale_price: 23,
       }));
@@ -64,72 +64,9 @@ search.addWidgets([
               {{name.en}}
             </h1>
             <div>            
-              By <strong>{{brand}}</strong> in <strong>{{category}}</strong>
+              <strong>{{sale}}</strong>
             </div>
           </div>
-
-          <div
-            style="
-              display: grid;
-              grid-auto-flow: column;
-              justify-content: start;
-              align-items: center;
-              gap: 8px;
-            "
-          >
-            {{#rating}}
-              <div
-                style="
-                  display: grid;
-                  grid-auto-flow: column;
-                  justify-content: start;
-                  align-items: center;
-                  gap: 4px;
-                "
-              >
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="#e2a400"
-                  stroke="#e2a400"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-                </svg>
-                {{rating}}
-              </div>
-            {{/rating}}
-
-            <div
-              style="
-                display: grid;
-                grid-auto-flow: column;
-                justify-content: start;
-                align-items: center;
-                gap: 4px;
-              "
-            >
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                style="
-                  position: relative;
-                  top: 1px;
-                "
-              >
-                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-              </svg>
-              <span>{{comments}}</span>
-            </div>
           </div>
         </article>
       `,
